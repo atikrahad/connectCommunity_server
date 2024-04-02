23cf08b781840c31720d9d565a2925b1529b4ed5
unknown <atikrahad1@gmail.com>
2024-04-02 10:47:46 +0600

    adding goups model

---
+++ Models/groups.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const groupScema = mongoose.Schema(
+  {
+    groupName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    members: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+      },
+    ],
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    createAt: {
+      type: Date,
+      default: new Date(),
+    },
+    groupPic: {
+      type: String,
+      default: "https://i.ibb.co/2kdqktN/istockphoto-589440682-612x612.jpg",
+    },
+    groupCover: {
+      type: String,
+      default:
+        "https://i.ibb.co/ZdPj5jx/group-diverse-people-holding-blank-banners-107791-11880.jpg",
+    },
+  },
+  { timestamps: true }
+);
+
+const group = mongoose.model("group", groupScema)
+
+module.exports = group
