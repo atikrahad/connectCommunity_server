33a1fc2b43891a6b9c0783a3ce1e9eec0f5758fb
unknown <atikrahad1@gmail.com>
2024-04-04 14:38:43 +0600

    adding group image and cover imag update api

--- Routes/groupRoutes.js
+++ Routes/groupRoutes.js
@@ -58,13 +58,91 @@ group.put("/addmembers/:id", async (req, res) => {
   const admin = req.body.addminId;
   const neweMember = req.body.newMember;
 
-  const group = await groupModel.findOneAndUpdate(
-    { _id: groupId, groupAdmin: [admin] },
-    {$push: {
-      members: neweMember
-    }}
-  );
-res.status(200).json(group)
+  const group = await groupModel
+    .findOneAndUpdate(
+      { _id: groupId, groupAdmin: [admin] },
+      {
+        $push: {
+          members: neweMember,
+        },
+      },
+      { new: true }
+    )
+    .populate("members", "name profilePic");
+  if (!group) {
+    res.status(400);
+    throw new Error("server error");
+  } else {
+    res.status(200).json(group);
+  }
+});
+
+group.put("/removemembers/:id", async (req, res) => {
+  const groupId = req.params.id;
+  const admin = req.body.addminId;
+  const removeMember = req.body.removeMember;
+
+  const group = await groupModel
+    .findOneAndUpdate(
+      { _id: groupId, groupAdmin: [admin] },
+      {
+        $pull: {
+          members: removeMember,
+        },
+      },
+      { new: true }
+    )
+    if (!group) {
+      res.status(400);
+      throw new Error("server error");
+    } else {
+      res.status(200).json(group);
+    }
+});
+
+group.put("/updategroupcover/:id", async (req, res) => {
+  const groupId = req.params.id;
+  const admin = req.body.addminId;
+  const newGroupCover = req.body.groupCover;
+
+  const group = await groupModel
+    .findOneAndUpdate(
+      { _id: groupId, groupAdmin: [admin] },
+      {
+        $set: {
+          groupCover: newGroupCover,
+        },
+      },
+      { new: true }
+    )
+    if (!group) {
+      res.status(400);
+      throw new Error("server error");
+    } else {
+      res.status(200).json(group);
+    }
+});
+group.put("/updategrouppic/:id", async (req, res) => {
+  const groupId = req.params.id;
+  const admin = req.body.addminId;
+  const newGroupPic = req.body.groupPic;
+
+  const group = await groupModel
+    .findOneAndUpdate(
+      { _id: groupId, groupAdmin: [admin] },
+      {
+        $set: {
+          groupPic: newGroupPic,
+        },
+      },
+      { new: true }
+    )
+    if (!group) {
+      res.status(400);
+      throw new Error("server error");
+    } else {
+      res.status(200).json(group);
+    }
 });
 
 module.exports = group;
